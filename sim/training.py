27cd2214aeabc14bf4ff95f1e4a69753707a9e35
Ronnie Wang <geniusxiaoguai@gmail.com>
2015-03-01 23:10:21 +0100

    modified training parameters, empirically.

--- sim/training.py
+++ sim/training.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 from numpy.linalg import norm
 
+tp='anime'
 
 #fr = open('dat/db.dat','rb')
 #imask = cPickle.load(fr)
@@ -12,19 +13,23 @@ from numpy.linalg import norm
 #cPickle.load(fr)
 #sm = cPickle.load(fr)
 #tm = cPickle.load(fr)
-fr.close()
-fr = open('dat/training.dat','rb')
+#fr.close()
+fr = open('dat/training-'+tp+'.dat','rb')
 S = cPickle.load(fr)
 St = cPickle.load(fr)
 cPickle.load(fr)
 States = cPickle.load(fr)
+Bu = cPickle.load(fr)
+Bi = cPickle.load(fr)
+U = cPickle.load(fr)
+Vt = cPickle.load(fr)
 fr.close()
-
-f = h5py.File("data.hdf5","r")
-U = f['u']
-Vt = f['vt']
-Bu = f['Buser']
-Bi = f['Bitem']
+States=States.tolil()
+#f = h5py.File("data.hdf5","r")
+#U = f['u']
+#Vt = f['vt']
+#Bu = f['Buser']
+#Bi = f['Bitem']
 
 def evaluate(S):
     """
@@ -39,9 +44,8 @@ def evaluate(S):
     L = S.getnnz()
     rtn = 0
     for i in xrange(L):
-        s = States[i,j]
+        s = States[I[i],J[i]]
         rtn+=(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])**2
-    rtn+=(norm(U, axis=1).sum()+norm(Vt, axis=0).sum()+norm(Bu, axis=1)+norm(Bi,axis=1))/2
     return rtn
 
 def derivative(S):
@@ -52,32 +56,42 @@ def derivative(S):
     Bu: M x 5 users states bias
     Bi: N x 5 items states bias
     """
-    dU = np.zeros((S.shape[0],500),dtype=np.float32)
-    dVt = np.zeros((500, S.shape[1]),dtype=np.float32)
-    dBu = np.zeros((S.shape[0],5),dtype=np.float32)
-    dBi = np.zeros((S.shape[1],5),dtype=np.float32)
+    dU = np.zeros((S.shape[0],800))
+    dVt = np.zeros((800, S.shape[1]))
+    dBu = np.zeros((S.shape[0],5))
+    dBi = np.zeros((S.shape[1],5))
     I,J = S.nonzero()
     L = S.getnnz()
 
     for i in xrange(L):
-        s = States[i,j]
+        s = States[I[i],J[i]]
         dU[I[i],:]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])*Vt[:,J[i]].T
         dVt[:,J[i]]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])*U[I[i],:].T
         dBu[I[i],s]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])
         dBi[J[i],s]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])
-    dBu+=Bu
-    dBi+=Bi
-    dU+=U
-    dVt+=Vt
+    dBu+=0.1*Bu
+    dBi+=0.1*Bi
+    dU+=0.1*U
+    dVt+=0.1*Vt
     return (dU ,dVt, dBu, dBi)
 
 if __name__=='__main__':
-    print evaluate(S)
-    print evaluate(St)
-    du, dvt, dbu, dbi = derivative(S)
-    U-=0.01*dU
-    Vt-=0.01*dVt
-    Bu-=0.01*dBu
-    Bi-=0.01*dBi
-    print evaluate(S)
-    print evaluate(St)
+    rtn =[]
+    crtn = []
+    for i in xrange(100):
+        rtn.append(evaluate(S))
+        crtn.append(evaluate(St))
+        print rtn[i]
+        print crtn[i]
+        dU ,dVt, dBu, dBi = derivative(S)
+        U-=0.0002*dU
+        Vt-=0.0002*dVt
+        Bu-=0.0002*dBu
+        Bi-=0.0002*dBi
+
+    fw = open('dat/temp-'+tp+'.dat','wb')
+    cPickle.dump(U,fw)
+    cPickle.dump(Vt,fw)
+    cPickle.dump(Bu,fw)
+    cPickle.dump(Bi,fw)
+    fw.close()
