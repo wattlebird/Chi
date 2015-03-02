742c376bdeae726822ad0d8359614ccc7dba1cf4
Ronnie Wang <geniusxiaoguai@gmail.com>
2015-03-03 00:14:58 +0100

    Using multithreading to acc training

--- sim/training.py
+++ sim/training.py
@@ -2,6 +2,9 @@ import cPickle
 import numpy as np
 import h5py
 from numpy.linalg import norm
+from numpy import sqrt
+import multiprocessing
+import time
 
 tp='anime'
 
@@ -17,7 +20,7 @@ tp='anime'
 fr = open('dat/training-'+tp+'.dat','rb')
 S = cPickle.load(fr)
 St = cPickle.load(fr)
-cPickle.load(fr)
+Sv = cPickle.load(fr)
 States = cPickle.load(fr)
 Bu = cPickle.load(fr)
 Bi = cPickle.load(fr)
@@ -46,52 +49,70 @@ def evaluate(S):
     for i in xrange(L):
         s = States[I[i],J[i]]
         rtn+=(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])**2
-    return rtn
+    return sqrt(rtn/L)
 
-def derivative(S):
-    """
-    S: M x N lil sparse matrix
-    U: M x 500 users vectors
-    Vt 500 x N items vectors 
-    Bu: M x 5 users states bias
-    Bi: N x 5 items states bias
-    """
+def derivative1(S):
     dU = np.zeros((S.shape[0],800))
     dVt = np.zeros((800, S.shape[1]))
-    dBu = np.zeros((S.shape[0],5))
-    dBi = np.zeros((S.shape[1],5))
     I,J = S.nonzero()
     L = S.getnnz()
-
     for i in xrange(L):
         s = States[I[i],J[i]]
         dU[I[i],:]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])*Vt[:,J[i]].T
         dVt[:,J[i]]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])*U[I[i],:].T
+    dU+=0.1*U
+    dVt+=0.1*Vt
+    return (dU ,dVt)
+
+def derivative2(S):
+    dBu = np.zeros((S.shape[0],5))
+    dBi = np.zeros((S.shape[1],5))
+    I,J = S.nonzero()
+    L = S.getnnz()
+    for i in xrange(L):
+        s = States[I[i],J[i]]
         dBu[I[i],s]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])
         dBi[J[i],s]+=-2*(S[I[i],J[i]]-U[I[i],:].dot(Vt[:,J[i]])-Bu[I[i],s]-Bi[J[i],s])
     dBu+=0.1*Bu
     dBi+=0.1*Bi
-    dU+=0.1*U
-    dVt+=0.1*Vt
-    return (dU ,dVt, dBu, dBi)
+    return (dBu, dBi)
+    
+    
+def worker1(d,St, S):
+    d["mse_validate"]=evaluate(St)
+    dU ,dVt = derivative1(S)
+    d['dU']=dU
+    d['dVt']=dVt
+    
+def worker2(d,S):
+    d["mse_test"]=evaluate(S)
+    dBu, dBi = derivative2(S)
+    d['dBu'] = dBu
+    d['dBi'] = dBi
+    
 
 if __name__=='__main__':
-    rtn =[]
-    crtn = []
-    for i in xrange(100):
-        rtn.append(evaluate(S))
-        crtn.append(evaluate(St))
-        print rtn[i]
-        print crtn[i]
-        dU ,dVt, dBu, dBi = derivative(S)
-        U-=0.0002*dU
-        Vt-=0.0002*dVt
-        Bu-=0.0002*dBu
-        Bi-=0.0002*dBi
+    try:
+        for i in xrange(200):
+            mgr = multiprocessing.Manager()
+            d = mgr.dict()
+            p1 = multiprocessing.Process(target=worker1, args=(d,St,S))
+            p2 = multiprocessing.Process(target=worker2, args=(d,S))
 
-    fw = open('dat/temp-'+tp+'.dat','wb')
-    cPickle.dump(U,fw)
-    cPickle.dump(Vt,fw)
-    cPickle.dump(Bu,fw)
-    cPickle.dump(Bi,fw)
-    fw.close()
+            p1.start()
+            p2.start()
+            p1.join()
+            p2.join()
+            print d['mse_test']
+            print d['mse_validate']
+            U-=0.0002*d['dU']
+            Vt-=0.0002*d['dVt']
+            Bu-=0.0002*d['dBu']
+            Bi-=0.0002*d['dBi']
+    finally:
+        fw = open('dat/temp-'+tp+'.dat','wb')
+        cPickle.dump(U,fw)
+        cPickle.dump(Vt,fw)
+        cPickle.dump(Bu,fw)
+        cPickle.dump(Bi,fw)
+        fw.close()
